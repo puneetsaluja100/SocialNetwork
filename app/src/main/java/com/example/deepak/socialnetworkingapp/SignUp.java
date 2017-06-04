61a09af455c214c2f0a07410ba472beff864a92b
Deepak <trigodeepak@gmail.com>
2017-06-04 12:15:38 +0530

    Added Login and Signup activity

---
+++ app/src/main/java/com/example/deepak/socialnetworkingapp/SignUp.java
@@ -0,0 +1,24 @@
+package com.example.deepak.socialnetworkingapp;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+
+    }
+}
+
