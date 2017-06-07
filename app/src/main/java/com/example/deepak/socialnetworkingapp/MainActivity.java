8b709227ea32b6dbf1dd317139d451692fec142c
Deepak Yadav <trigodeepak@gmail.com>
2017-06-06 17:21:39 -0700

    Json Parsing

--- app/src/main/java/com/example/deepak/socialnetworkingapp/MainActivity.java
+++ app/src/main/java/com/example/deepak/socialnetworkingapp/MainActivity.java
@@ -8,6 +8,9 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,22 +39,45 @@ public class MainActivity extends AppCompatActivity {
         post = new Post(2,"HI there ","deepak.jpg","9999-12-31 23:59:59");
         postList.add(post);
 
-        LoaderManager.LoaderCallbacks<List<Post>> loaderCallbacks = new LoaderManager.LoaderCallbacks<List<Post>>() {
-            @Override
-            public Loader<List<Post>> onCreateLoader(int id, Bundle args) {
-                PostLoader loader = new PostLoader(getApplicationContext());
-                return loader;
-            }
-
-            @Override
-            public void onLoadFinished(Loader<List<Post>> loader, List<Post> data) {
-                mAdapter.swapdata(data);
-            }
-
-            @Override
-            public void onLoaderReset(Loader<List<Post>> loader) {
-                mAdapter.swapdata(null);
-            }
+//        String result = "[{\"id\":\"1\",\"profile_id\":\"5\",\"post_text\":\"Hi op !\",\"post_image\":\"0\",\"post_time\":\"2017-06-05 16:59:33\"},{\"id\":\"2\",\"profile_id\":\"6\",\"post_text\":\"HI Deepak !\",\"post_image\":\"0\",\"post_time\":\"2017-06-05 17:08:07\"},{\"id\":\"3\",\"profile_id\":\"2\",\"post_text\":\"HI PUNEET!\",\"post_image\":\"0\",\"post_time\":\"2017-06-05 17:08:44\"}]";
+//
+//        postList = parseResult(result);}
+//
+//        public List<Post> parseResult(String result) {
+//            List<Post> postList = null;
+//            Post post = null;
+//            try {
+//                JSONObject reader = new JSONObject(result);
+//
+//                post.setProfileId(Integer.parseInt(reader.getString("id")));
+//                post.setPostImage(reader.getString("post_image"));
+//                post.setPostText( reader.getString("post_text") );
+//                post.setProfileId( Integer.parseInt( reader.getString( "profile_id" ) ) );
+//                post.setPostTime(reader.getString( "post_time" ));
+//                postList.add(post);
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//            return postList;
         }
-    }
+
+//        LoaderManager.LoaderCallbacks<List<Post>> loaderCallbacks = new LoaderManager.LoaderCallbacks<List<Post>>() {
+//            @Override
+//            public Loader<List<Post>> onCreateLoader(int id, Bundle args) {
+//
+//                PostLoader loader = new PostLoader(getApplicationContext(),new String[] {"post"});
+//                return loader;
+//            }
+//
+//            @Override
+//            public void onLoadFinished(Loader<List<Post>> loader, List<Post> data) {
+//                mAdapter.swapdata(data);
+//            }
+//
+//            @Override
+//            public void onLoaderReset(Loader<List<Post>> loader) {
+//                mAdapter.swapdata(null);
+//            }
+//        }
+
 }
